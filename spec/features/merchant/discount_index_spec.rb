cb6bbedefb1a6988e2253761dcafe6848d4cff64
Michael Evans <michaeljevans113@gmail.com>
2020-08-02 11:46:40 -0600

    Fix syntax

--- spec/features/merchant/discount_index_spec.rb
+++ spec/features/merchant/discount_index_spec.rb
@@ -4,17 +4,17 @@ RSpec.describe 'Merchant discount index' do
   it 'displays all discounts created by the merchant' do
     cory  = Merchant.create!(name: "Cory's Coffee", address: '456 North St', city: 'Denver', state: 'CO', zip: 12345)
     cory_merch = User.create!(name: 'Cory', address: "456 Don't Worry About It Dr", city: 'Denver', state: 'CO', zip: 12345, email: 'cory@me.com', password: 'coffee', role: 1, merchant_id: cory.id)
-    discount = Discount.create!(percentage: 10, items_required: 10)
+    discount = Discount.create!(percentage: 10, items_required: 10, merchant_id: cory.id)
 
     visit '/login'
     fill_in :email, with: cory_merch.email
     fill_in :password, with: cory_merch.password
-    click 'Log In'
+    click_button 'Log In'
 
     expect(current_path).to eq('/merchant')
 
     expect(page).to have_content('View Current Discounts')
-    click 'View Current Discounts'
+    click_on 'View Current Discounts'
 
     expect(current_path).to eq('/merchant/discounts')
 

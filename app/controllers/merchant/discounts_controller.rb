4954eae91db2511796a5ea83b91a36add6e2789a
Michael Evans <michaeljevans113@gmail.com>
2020-08-02 16:46:05 -0600

    Add create action

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -6,4 +6,22 @@ class Merchant::DiscountsController < Merchant::BaseController
   def show
     @discount = Discount.find(params[:id])
   end
+
+  def new
+    @discount = Discount.new
+  end
+
+  def create
+    discount = Merchant.find(params[:merchant_id]).discounts.new(discount_params)
+    if discount.save
+      flash[:success] = 'Discount created!'
+      redirect_to '/merchant/discounts'
+    end
+  end
+
+  private
+
+  def discount_params
+    params.require(:discount).permit(:percentage, :items_required)
+  end
 end

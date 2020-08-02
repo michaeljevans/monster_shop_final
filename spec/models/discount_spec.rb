51528b4b7762665ba64377bd9521cc88d2865665
Michael Evans <michaeljevans113@gmail.com>
2020-08-02 12:04:24 -0600

    Add test for .by_percentage

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -13,4 +13,15 @@ RSpec.describe Discount do
     it {should validate_numericality_of(:items_required)
           .only_integer}
   end
+
+  describe 'class methods' do
+    it '.by_percentage' do
+      cory  = Merchant.create!(name: "Cory's Coffee", address: '456 North St', city: 'Denver', state: 'CO', zip: 12345)
+      discount_1 = Discount.create!(percentage: 30, items_required: 10, merchant_id: cory.id)
+      discount_2 = Discount.create!(percentage: 20, items_required: 10, merchant_id: cory.id)
+      discount_3 = Discount.create!(percentage: 50, items_required: 10, merchant_id: cory.id)
+
+      expect(Discount.all.by_percentage).to eq([discount_3, discount_1, discount_2])
+    end
+  end
 end

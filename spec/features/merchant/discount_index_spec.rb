788e17f73738e8cd545b58648402b7d44dd66977
Michael Evans <michaeljevans113@gmail.com>
2020-08-02 15:19:24 -0600

    Add expectations to ensure that merchants do not see/have access to discounts created by other merchants

--- spec/features/merchant/discount_index_spec.rb
+++ spec/features/merchant/discount_index_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe 'Merchant discount index' do
   it 'displays all discounts created by the merchant' do
     cory  = Merchant.create!(name: "Cory's Coffee", address: '456 North St', city: 'Denver', state: 'CO', zip: 12345)
+    brian = Merchant.create!(name: "Brian's Bazaar", address: '789 South St', city: 'Denver', state: 'CO', zip: 12345)
     cory_merch = User.create!(name: 'Cory', address: "456 Don't Worry About It Dr", city: 'Denver', state: 'CO', zip: 12345, email: 'cory@me.com', password: 'coffee', role: 1, merchant_id: cory.id)
+    brian_merch = User.create!(name: 'Brian', address: '789 Beeswax, Not Yours Blvd', city: 'Denver', state: 'CO', zip: 12345, email: 'brian@me.com', password: 'bazaar', role: 1, merchant_id: brian.id)
     discount = Discount.create!(percentage: 10, items_required: 10, merchant_id: cory.id)
 
     visit '/login'
@@ -24,5 +26,17 @@ RSpec.describe 'Merchant discount index' do
       expect(page).to have_content("Percentage: #{discount.percentage}%")
       expect(page).to have_content("Items Required: #{discount.items_required}")
     end
+
+    visit '/logout'
+
+    visit '/login'
+    fill_in :email, with: brian_merch.email
+    fill_in :password, with: brian_merch.password
+    click_button 'Log In'
+
+    click_on 'View Current Discounts'
+
+    expect(page).to_not have_content('Percentage:')
+    expect(page).to_not have_content('Items Required:')
   end
 end

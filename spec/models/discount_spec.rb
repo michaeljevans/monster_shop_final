fdce41348729af117718f8620ab75312e710ce9f
Michael Evans <michaeljevans113@gmail.com>
2020-08-02 11:28:52 -0600

    Add validations

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -6,7 +6,11 @@ RSpec.describe Discount do
   end
 
   describe 'Validations' do
-    it {should validate_presence_of :percentage}
-    it {should validate_presence_of :items_required}
+    it {should validate_numericality_of(:percentage)
+          .is_less_than(100)
+          .is_greater_than(0)
+          .only_integer}
+    it {should validate_numericality_of(:items_required)
+          .only_integer}
   end
 end

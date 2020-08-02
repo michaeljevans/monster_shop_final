a29d450dcfe2f9d8d5f072bd441f88af57fcaf39
Michael Evans <michaeljevans113@gmail.com>
2020-08-02 14:06:23 -0600

    Add show action

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -2,4 +2,8 @@ class Merchant::DiscountsController < Merchant::BaseController
   def index
     @discounts = Merchant.find(current_user.merchant_id).discounts
   end
+
+  def show
+    @discount = Discount.find(params[:id])
+  end
 end

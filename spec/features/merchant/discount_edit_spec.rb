d6cdb8731429e4d27c728edec893eb3784eb55c6
Michael Evans <michaeljevans113@gmail.com>
2020-08-03 15:49:15 -0600

    Create merchant discount edit spec

---
+++ spec/features/merchant/discount_edit_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant discount index' do
+  it 'allows the merchant to update discount attributes' do
+    cory  = Merchant.create!(name: "Cory's Coffee", address: '456 North St', city: 'Denver', state: 'CO', zip: 12345)
+    cory_merch = User.create!(name: 'Cory', address: "456 Don't Worry About It Dr", city: 'Denver', state: 'CO', zip: 12345, email: 'cory@me.com', password: 'coffee', role: 1, merchant_id: cory.id)
+    discount = Discount.create!(percentage: 10, items_required: 10, merchant_id: cory.id)
+
+    visit '/login'
+    fill_in :email, with: cory_merch.email
+    fill_in :password, with: cory_merch.password
+    click_button 'Log In'
+
+    click_on 'View Current Discounts'
+
+    within ".discount-#{discount.id}" do
+      click_button 'Edit Discount'
+    end
+
+    expect(current_path).to eq("/merchant/discounts/#{discount.id}/edit")
+
+    fill_in 'Items Required', with: 20
+    click_button 'Update Discount'
+
+    expect(current_path).to eq("/merchant/discounts/#{discount.id}")
+
+    expect(page).to have_content('Items Required for Discount: 20')
+    expect(page).to_not have_content('Items Required for Discount: 10')
+  end
+end

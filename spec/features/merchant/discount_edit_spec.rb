699a7ca84991b59ae9fd5376ed225e0c5291ee40
Michael Evans <michaeljevans113@gmail.com>
2020-08-03 17:48:51 -0600

    Add test for sad path

--- spec/features/merchant/discount_edit_spec.rb
+++ spec/features/merchant/discount_edit_spec.rb
@@ -27,4 +27,27 @@ RSpec.describe 'Merchant discount index' do
     expect(page).to have_content('Items Required for Discount: 20')
     expect(page).to_not have_content('Items Required for Discount: 10')
   end
+
+  it 'displays an error for incomplete discount information' do
+    cory  = Merchant.create!(name: "Cory's Coffee", address: '456 North St', city: 'Denver', state: 'CO', zip: 12345)
+    cory_merch = User.create!(name: 'Cory', address: "456 Don't Worry About It Dr", city: 'Denver', state: 'CO', zip: 12345, email: 'cory@me.com', password: 'coffee', role: 1, merchant_id: cory.id)
+    discount = Discount.create!(percentage: 10, items_required: 10, merchant_id: cory.id)
+
+    visit '/login'
+    fill_in :email, with: cory_merch.email
+    fill_in :password, with: cory_merch.password
+    click_button 'Log In'
+
+    click_on 'View Current Discounts'
+
+    within ".discount-#{discount.id}" do
+      click_button 'Edit Discount'
+    end
+
+    fill_in 'Percentage', with: ''
+    fill_in 'Items Required', with: 20
+    click_button 'Update Discount'
+
+    expect(page).to have_content('All fields are required to update discount information.')
+  end
 end
